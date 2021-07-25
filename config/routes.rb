662389f7b96ec4c1a02ee22044253c13ded9ab5a
hi-roto <hirostudy2020@gmail.com>
2021-07-26 00:18:26 +0900

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "tweets#index"
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new, :create]
   # get '/tweets', to: "tweets#index"
+  # get '/tweets/new', to: "tweets#new"
+  # post "/tweets", to: "tweets#create"
 end
